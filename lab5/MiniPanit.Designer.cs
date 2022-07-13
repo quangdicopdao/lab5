3638a95e4857489b028c47fbee22521390868f61
Quang <Quang@DESKTOP-RA3BQ7B>
2022-07-13 08:55:04 +0700

    Add project files.

---
+++ lab5/MiniPanit.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace lab5
+{
+    partial class MiniPanit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MiniPanit));
+            this.panelSlide = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPen = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.cboLevel = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.picDraw = new System.Windows.Forms.PictureBox();
+            this.panelSlide.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSlide
+            // 
+            this.panelSlide.Controls.Add(this.btnClear);
+            this.panelSlide.Controls.Add(this.btnPen);
+            this.panelSlide.Controls.Add(this.btnDelete);
+            this.panelSlide.Controls.Add(this.btnColor);
+            this.panelSlide.Controls.Add(this.cboLevel);
+            this.panelSlide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSlide.Location = new System.Drawing.Point(0, 0);
+            this.panelSlide.Name = "panelSlide";
+            this.panelSlide.Size = new System.Drawing.Size(229, 681);
+            this.panelSlide.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClear.BackgroundImage")));
+            this.btnClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(45, 518);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 74);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPen
+            // 
+            this.btnPen.BackColor = System.Drawing.Color.White;
+            this.btnPen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPen.BackgroundImage")));
+            this.btnPen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPen.Location = new System.Drawing.Point(45, 244);
+            this.btnPen.Name = "btnPen";
+            this.btnPen.Size = new System.Drawing.Size(132, 74);
+            this.btnPen.TabIndex = 1;
+            this.btnPen.UseVisualStyleBackColor = false;
+            this.btnPen.Click += new System.EventHandler(this.btnPen_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.White;
+            this.btnDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDelete.BackgroundImage")));
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Location = new System.Drawing.Point(45, 379);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(132, 74);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.White;
+            this.btnColor.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnColor.BackgroundImage")));
+            this.btnColor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnColor.FlatAppearance.BorderSize = 0;
+            this.btnColor.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnColor.Location = new System.Drawing.Point(45, 105);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(132, 74);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // cboLevel
+            // 
+            this.cboLevel.FormattingEnabled = true;
+            this.cboLevel.Location = new System.Drawing.Point(33, 40);
+            this.cboLevel.Name = "cboLevel";
+            this.cboLevel.Size = new System.Drawing.Size(161, 24);
+            this.cboLevel.TabIndex = 0;
+            this.cboLevel.Text = "10";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.picDraw);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(229, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 681);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnOpen);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 598);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(756, 83);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSave.Location = new System.Drawing.Point(191, 23);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 48);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnOpen.Location = new System.Drawing.Point(395, 23);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(124, 48);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // picDraw
+            // 
+            this.picDraw.BackColor = System.Drawing.Color.White;
+            this.picDraw.Dock = System.Windows.Forms.DockStyle.Top;
+            this.picDraw.Location = new System.Drawing.Point(0, 0);
+            this.picDraw.Name = "picDraw";
+            this.picDraw.Size = new System.Drawing.Size(756, 592);
+            this.picDraw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDraw.TabIndex = 0;
+            this.picDraw.TabStop = false;
+            this.picDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.picDraw_Paint);
+            this.picDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseDown);
+            this.picDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseMove);
+            this.picDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picDraw_MouseUp);
+            // 
+            // MiniPanit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelSlide);
+            this.Name = "MiniPanit";
+            this.Text = "Mini Pant Program";
+            this.Load += new System.EventHandler(this.MiniPanit_Load);
+            this.panelSlide.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picDraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSlide;
+        private System.Windows.Forms.ComboBox cboLevel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picDraw;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPen;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ lab5/MiniPanit.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace lab5
+{
+    public partial class MiniPanit : Form
+    {
+        
+        public MiniPanit()
+        {
+            InitializeComponent();
+            bit = new Bitmap(picDraw.Width, picDraw.Height);
+            g = Graphics.FromImage(bit);
+            g.Clear(Color.White);
+            picDraw.Image = bit;
+        }
+
+        Graphics g;
+        bool StartPaint = false;
+        bool Del = false;
+        Bitmap bit;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 5);
+        private void MiniPanit_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 10; i++)
+                cboLevel.Items.Add(i.ToString());
+        }
+
+        private void picDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (StartPaint)
+            {
+                if (!Del)
+                {
+                    px = e.Location;
+                    Pen p = new Pen(btnColor.BackColor,float.Parse(cboLevel.Text));
+                    g.DrawLine(p, px, py);
+                    py = px;
+                }
+                else 
+                {
+                    px = e.Location;
+                    Pen p = new Pen(Color.White, float.Parse(cboLevel.Text));
+                    g.DrawLine(p, px, py);
+                    py = px;
+                }
+            }
+
+            
+            picDraw.Refresh();
+            
+        }
+        
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if(color.ShowDialog() == DialogResult.OK)
+            {
+                btnColor.BackColor = color.Color;
+            }
+        }
+
+        private void btnPen_Click(object sender, EventArgs e)
+        {
+            StartPaint = true;
+            Del = false;    
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Del = true;
+            StartPaint = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(picDraw.BackColor);
+            StartPaint = false;
+            Del = false;
+        }
+
+        private void picDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            StartPaint = false;
+            
+        }
+
+        private void picDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            StartPaint = true;
+            py = e.Location;
+        }
+
+        
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Image(*.jpg)|*.jpg";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap btm =  bit.Clone(new Rectangle(0,0,picDraw.Width,picDraw.Height),bit.PixelFormat);
+                btm.Save(sfd.FileName,  ImageFormat.Jpeg);
+                MessageBox.Show("Image saved!!!");
+            }
+        }
+
+        private void picDraw_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+        }
+
+        
+    }
+}
